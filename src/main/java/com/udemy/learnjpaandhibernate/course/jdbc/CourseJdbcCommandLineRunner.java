08af0536e47a0c0d7bcbeb3a498ec904f4b5ea69
sunoyu <suno_yu@naver.com>
2023-12-21 18:35:15 +0900

    스프링 JDBC 기본 세팅

---
+++ src/main/java/com/udemy/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -0,0 +1,16 @@
+package com.udemy.learnjpaandhibernate.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component   // 컴포넌트가 있어야 Springboot가 스캔하여 사용함.
+public class CourseJdbcCommandLineRunner implements CommandLineRunner {
+    @Autowired
+    private CourseJdbcRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        repository.insert();
+    }
+}
---
+++ src/main/java/com/udemy/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
@@ -0,0 +1,20 @@
+package com.udemy.learnjpaandhibernate.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CourseJdbcRepository {
+    @Autowired
+    private JdbcTemplate springJdbcTemplate;
+    private static String INSERT_QUERY =
+            """
+                    insert into course (id, name, author)
+                    values(1, 'Learn AWS', 'in28munutes');
+            """;
+
+    public void insert() {
+        springJdbcTemplate.update(INSERT_QUERY);
+    }
+}
